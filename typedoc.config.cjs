43489da69a294c4855e75d88ce5ab633a9110930
Jameslinimk <jameskimimk@gmail.com>
2023-03-07 20:25:26 -0500

    qwe

---
+++ typedoc.config.cjs
@@ -0,0 +1,14 @@
+const { readdirSync } = require("fs")
+const { join } = require("path")
+
+const dirs = (source) =>
+	readdirSync(join(__dirname, source), { withFileTypes: true })
+		.filter((dirent) => dirent.isDirectory())
+		.map((dirent) => dirent.name)
+
+/** @type {import("typedoc").TypeDocOptions} */
+module.exports = {
+	entryPoints: dirs("src/lib").map((dir) => `./src/lib/${dir}/*.ts`),
+	out: "./static/docs",
+	readme: "./READMEDOC.md",
+}
