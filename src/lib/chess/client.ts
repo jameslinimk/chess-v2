02cffe6620e7e82a9376ae94eb752d273de33fdf
Jameslinimk <jameskimimk@gmail.com>
2023-03-15 21:23:30 -0400

    st

--- src/lib/chess/client.ts
+++ src/lib/chess/client.ts
@@ -1,4 +1,5 @@
+import { dev } from "$app/environment"
 import type { ClientToServerEvents, ServerToClientEvents } from "$lib/server/socket.js"
 import { io, type Socket } from "socket.io-client"
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io()
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(dev ? "http://localhost:3000" : "https://chessv2.jamesalin.com")
